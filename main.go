c456dbc20fada78d7ad8f15c88624ab8d7119af0
itza2k <161753369+itza2k@users.noreply.github.com>
2025-01-01 23:34:43 +0530

    channels

--- main.go
+++ main.go
@@ -17,20 +17,35 @@ func main() {
 	fmt.Printf("Name: %s\n", name)
 	fmt.Printf("Age: %d\n", age)
 	fmt.Printf("Learning: %s\n", language)
-	fmt.Printf("Currently Learning: %v\n", isLearning)
+	fmt.Printf("Currently Learning: %v\n\n", isLearning)
 
 	// concurrent function call
-	go greetConcurrently("Hello from a goroutine!!!")
+	go greetConcurrently("Hello from a goroutine!")
 
 	// thread work - main
 	fmt.Println("Main function is doing other tasks...")
 
+	// channels
+	resultChan := make(chan int)
+	go sumNumbers(3, 5, resultChan)
+
 	// goroutine to complete
 	time.Sleep(1 * time.Second)
 	fmt.Println("Program is ending.")
+
+	// channel result and print
+	sumResult := <-resultChan
+	fmt.Printf("Received sum from channel: %d\n", sumResult)
 }
 
+// sleep and print
 func greetConcurrently(message string) {
 	time.Sleep(500 * time.Millisecond)
 	fmt.Println(message)
+}
+
+// sleep and sum to channel
+func sumNumbers(a, b int, c chan int) {
+	time.Sleep(200 * time.Millisecond)
+	c <- a + b
 }
